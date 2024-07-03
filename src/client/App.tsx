7c3b6d2f04447949770160590025587f004574ca
Tianbo <zongtb1027@gmail.com>
2024-07-02 22:37:27 -0700

    feat: add tailwindcss config

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -9,11 +9,17 @@ function App(): JSX.Element {
     <Router>
       <div className={styles.App}>
         <header className={styles['App-header']}>
-          <p>
-            <button onClick={() => setCount((count) => count + 1)}>
+          <p className="w-[100px] h-[100px] bg-yellow-200">
+            <button
+              className=" bg-yellow-200"
+              onClick={() => setCount((count) => count + 1)}
+            >
               count is: {count}
             </button>
           </p>
+          <h1 className="text-3xl  font-bold underline text-yellow-400">
+            Hello world!
+          </h1>
           <Switch>
             <Route path="/about">
               <main>About</main>
--- src/server/router.ts
+++ src/server/router.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/hello', async (_req, res) => {
-  res.status(200).json({ message: 'Hello World1111111!' });
+  res.status(200).json({ message: 'Hello World' });
 });
 
 router.post('/add', async (_req, res) => {
